0b0de6c3e2256f61d7d8754a673e9a708f99f488
jmjuanes <josemijuanes@gmail.com>
2017-04-13 23:28:26 +0200

    index.js: added mgviz account provider

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var auth = require('electron-auth');
 
 //Authentication function
-module.exports = function(opt, callback)
+var openid = function(opt, callback)
 {
   //Check the options
   if(typeof opt !== 'object'){ throw new Error('No options provided'); }
@@ -10,5 +10,41 @@ module.exports = function(opt, callback)
   //Check the callback
   if(typeof callback !== 'function'){ throw new Error('No callback function provided'); }
 
-
+  //Do the authentication
+  return auth(openid._provider, opt, callback);
 };
+
+//Authorization url
+openid.url = 'https://mgviz15w.appspot.com/authorize?';
+
+//Provider object
+openid._provider = {};
+
+//Build the authorization url
+openid._provider.authorization_url = function(opt, cb)
+{
+  //Check the client value
+  if(typeof opt.client !== 'string'){ return cb(new Error('Client ID not provided'), null); }
+
+  //Check the allow signup value
+  if(typeof opt.allow_signup !== 'boolean'){ opt.allow_signup = false; }
+
+  //Build the authorization url
+  return openid.url + 'client=' + opt.client + '&' + 'allow_signup=' + opt.allow_signup.toString();
+};
+
+//Authorization done
+openid._provider.authorization_done = function(opt, url, window, cb)
+{
+  //Get the token value
+  var raw_token = /token=([^&]*)/.exec(url) || null;
+
+  //Get the real token value
+  var token = (raw_token && raw_token.length > 1) ? raw_token[1] : null;
+
+  //Check the token value and do the callback
+  if(token){ return cb(null, token); }
+};
+
+//Exports to node
+module.exports = openid;
