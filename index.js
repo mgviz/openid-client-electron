260f51cf266ce8363dfcaa5f46ceea5181649133
jmjuanes <josemijuanes@gmail.com>
2017-04-17 17:33:19 +0200

    index.js: big rewrite

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
 //Import dependencies
 var auth = require('electron-auth');
+var url = require('url');
 
-//Authentication function
-var openid = function(opt, callback)
-{
-  //Check the options
-  if(typeof opt !== 'object'){ throw new Error('No options provided'); }
-
-  //Check the callback
-  if(typeof callback !== 'function'){ throw new Error('No callback function provided'); }
-
-  //Do the authentication
-  return auth(openid._provider, opt, callback);
-};
-
-//Authorization url
-openid.url = 'https://mgviz15w.appspot.com/authorize?';
-
-//Provider object
-openid._provider = {};
+//Build the provider object
+var provider = {};
 
 //Build the authorization url
-openid._provider.authorization_url = function(opt, cb)
+provider.authorization_url = function(opt, cb)
 {
   //Check the client value
-  if(typeof opt.client !== 'string'){ return cb(new Error('Client ID not provided'), null); }
-
-  //Check the allow signup value
-  if(typeof opt.allow_signup !== 'boolean'){ opt.allow_signup = false; }
+  if(typeof opt.client !== 'string' || opt.client.trim() === ''){ return cb(new Error('Client ID not provided'), null); }
 
   //Build the authorization url
-  return openid.url + 'client=' + opt.client + '&' + 'allow_signup=' + opt.allow_signup.toString();
+  var auth_url = url.resolve(opt.url, 'authorize?client=' + opt.client + '&' + 'allow_signup=' + opt.allow_signup.toString());
+
+  //Do the callback
+  return cb(null, auth_url);
 };
 
 //Authorization done
-openid._provider.authorization_done = function(opt, url, window, cb)
+provider.authorization_done = function(opt, new_url, window, cb)
 {
   //Get the token value
-  var raw_token = /token=([^&]*)/.exec(url) || null;
+  var raw_token = /token=([^&]*)/.exec(new_url) || null;
 
   //Get the real token value
   var token = (raw_token && raw_token.length > 1) ? raw_token[1] : null;
 
   //Check the token value and do the callback
   if(token){ return cb(null, token); }
+};
+
+//Authentication function
+var openid = function(opt)
+{
+  //Check the options
+  if(typeof opt !== 'object'){ throw new Error('No options provided'); }
+
+  //Check the client value
+  if(typeof opt.client !== 'string'){ throw new Error('No client ID provided'); }
+
+  //Add the client id
+  this._client = opt.client.trim();
+
+  //Add the secret key
+  //this._secret = (typeof opt.secret === 'string') ? opt.secret.trim() : '';
+
+  //Add the allow signup value
+  this._allow_signup = (typeof opt.allow_signup === 'boolean') ? opt.allow_signup : false;
+
+  //Authorization url
+  this._url = (typeof opt.url === 'string') ? opt.url.trim() : 'https://mgviz15w.appspot.com';
+
+  //Return this
+  return this;
+};
+
+//Do the authentication
+openid.prototype.authorize = function(cb)
+{
+  //Check the callback
+  if(typeof cb !== 'function'){ throw new Error('No callback function provided'); }
+
+  //Do the authentication
+  return auth(provider, { client: this._client, url: this._url, allow_signup: this._allow_signup }, cb);
 };
 
 //Exports to node
